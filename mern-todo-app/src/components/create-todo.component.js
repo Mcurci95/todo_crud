0ff35f8c609d2fd736c19ee1905c69aa6c923e9f
Mcurci95 <mcurci95@gmail.com>
2020-02-14 23:13:05 -0800

    Initial constructor and state

--- mern-todo-app/src/components/create-todo.component.js
+++ mern-todo-app/src/components/create-todo.component.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 
 export default class CreateTodo extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            todo_description: '',
+            todo_responsible: '',
+            todo_priorty: '',
+            todo_complete: false
+        }
+    }
+
     render() {
         return (
             <div>
